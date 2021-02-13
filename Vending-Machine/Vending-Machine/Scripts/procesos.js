1c27d8d96b4dbea2e456f5e10f8c2159f36098db
maritza1 <maritzafunes098@outlook.es>
2021-02-12 22:04:06 -0600

    Creation of the functionalities of the buttons Order to put in the section Your Order the name and price of each product and total to pay

---
+++ Vending-Machine/Vending-Machine/Scripts/procesos.js
@@ -0,0 +1,36 @@
+﻿var btnsoda = document.getElementById('sodaB');
+var btnpapas = document.getElementById('papasB');
+var btncafe = document.getElementById('cafeB');
+
+var orden = document.getElementById('orden');
+var pagar = document.getElementById('pagar');
+
+var total = 0;
+
+function agregarSoda() {
+    var node = document.createElement("p");
+    var textnode = document.createTextNode("Pepsi - $ 1.50");
+    node.appendChild(textnode);
+    orden.appendChild(node);
+
+    total += 1.50;
+    pagar.innerHTML = "Total: $ " + total;
+}
+function agregarPapas() {
+    var node = document.createElement("p");
+    var textnode = document.createTextNode("Papas Sabritas- $ 0.50");
+    node.appendChild(textnode);
+    orden.appendChild(node);
+
+    total += 0.50;
+    pagar.innerHTML = "Total: $ " + total;
+}
+function agregarCafe() {
+    var node = document.createElement("p");
+    var textnode = document.createTextNode("Cafe negro- $ 0.75");
+    node.appendChild(textnode);
+    orden.appendChild(node);
+
+    total += 0.75;
+    pagar.innerHTML = "Total: $ " + total;
+}
